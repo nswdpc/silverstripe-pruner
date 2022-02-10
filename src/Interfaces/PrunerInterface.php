bd3da9161a97ceb2603880a499cadfd938f2dbc4
James (DPC) <james.ellis@dpc.nsw.gov.au>
2022-02-10 14:56:34 +1100

    Enforce return types in interface

--- src/Interfaces/PrunerInterface.php
+++ src/Interfaces/PrunerInterface.php
@@ -2,20 +2,22 @@
 
 namespace NSWDPC\Pruner;
 
+use SilverStripe\ORM\SS_List;
+
 /**
  * Interface for supporting classes that allow pruning via {@link NSWDPC\Pruner\Pruner}
  * @author James
  */
 interface PrunerInterface
 {
-    
+
     /**
      * Return a list of records to be pruned
      * @param int $daysAgo
      * @return DataList
      */
-    public function pruneList(int $days_ago, int $limit);
-    public function onBeforePrune();
-    public function onAfterPrune();
-    public function pruneFilesList();
+    public function pruneList(int $days_ago, int $limit) : SS_List;
+    public function onBeforePrune() : void;
+    public function onAfterPrune() : void;
+    public function pruneFilesList() : SS_List;
 }
