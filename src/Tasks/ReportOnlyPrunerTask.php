e0a01e11421f3e670a15aadddde074c493e2361b
JamesDPC <69664712+JamesDPC@users.noreply.github.com>
2026-04-21 23:29:05 +0000

    [php-cs-fixer] Automated updates generated by php-cs-fixer configuration

--- src/Tasks/ReportOnlyPrunerTask.php
+++ src/Tasks/ReportOnlyPrunerTask.php
@@ -3,7 +3,6 @@
 namespace NSWDPC\Pruner;
 
 use SilverStripe\Dev\BuildTask;
-use SilverStripe\ORM\DB;
 use SilverStripe\PolyExecution\PolyOutput;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
